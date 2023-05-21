1a3fbf1172cefe646411ec43e37ecc0804652e2d
MAVINGAyannis <85069723+MAVINGAyannis@users.noreply.github.com>
2023-05-22 00:11:56 +0200

    SSH connection to all hosts executed

--- mla.py
+++ mla.py
@@ -4,6 +4,7 @@ import json
 import paramiko
 from paramiko import SSHConfig
 import os
+import socket
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Script to process inventory and todos')
@@ -24,13 +25,17 @@ def establish_ssh_connection(host, port):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh.connect(host, port=port)
+        ssh.connect(hostname=host, port=port, timeout=5)  # Ajout du paramètre timeout
         print(f'SSH connection established with {host}')
         # Effectuez ici les opérations nécessaires sur l'hôte distant
         # ...
     except paramiko.AuthenticationException:
         print(f'Authentication failed for {host}')
     except paramiko.SSHException as e:
+        print(f'Error while establishing SSH connection with {host}: {str(e)}')
+    except socket.timeout:
+        print(f'Timeout while establishing SSH connection with {host}')
+    except socket.gaierror as e:
         print(f'Error while establishing SSH connection with {host}: {str(e)}')
     finally:
         ssh.close()
@@ -50,23 +55,17 @@ if __name__ == '__main__':
 
     # Lecture du fichier d'inventaire YAML
     inventory_data = read_yaml_file(args.inventory)
-    print('Contenu de l\'inventaire :')
-    print(json.dumps(inventory_data, indent=4))
 
     # Conversion en JSON et écriture dans un fichier
     inventory_json_path = 'inventory.json'
     write_json_file(inventory_data, inventory_json_path)
-    print(f'Inventaire converti en JSON et enregistré dans {inventory_json_path}')
 
     # Lecture du fichier de todos YAML
     todos_data = read_yaml_file(args.todos)
-    print('Liste des todos :')
-    print(json.dumps(todos_data, indent=4))
 
     # Conversion en JSON et écriture dans un fichier
     todos_json_path = 'todos.json'
     write_json_file(todos_data, todos_json_path)
-    print(f'Liste de todos convertie en JSON et enregistrée dans {todos_json_path}')
 
     # Chargement de la configuration SSH par défaut
     ssh_config = SSHConfig()
@@ -76,12 +75,12 @@ if __name__ == '__main__':
             ssh_config.parse(f)
 
     # Établissement de la connexion SSH avec chaque hôte
-    for host in inventory_data:
-        # Récupération des paramètres de configuration SSH pour l'hôte
-        ssh_config_host = ssh_config.lookup(host)
-        if 'port' in ssh_config_host:
-            port = int(ssh_config_host['port'])
-        else:
-            port = inventory_data[host].get('port', 22)
+    for host, host_info in inventory_data['hosts'].items():
+        address = host_info.get('ssh_address')
+        port = host_info.get('ssh_port', 22)
+        print(address, port)
 
-        establish_ssh_connection(host, port)
+        if address:
+            establish_ssh_connection(address, port)
+        else:
+            print(f'SSH address not found for host {host}')
