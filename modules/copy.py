08c56db5ca5a467023491de4cbd69965bd2c451f
MAVINGAyannis <85069723+MAVINGAyannis@users.noreply.github.com>
2023-05-27 02:59:47 +0200

    copy of a folder enabled

--- modules/copy.py
+++ modules/copy.py
@@ -1,26 +1,31 @@
 import paramiko
+import scp
+from scp import SCPClient
+import os
 
 def copy(client, params, host_info):
     src = params.get('src')
     dest = params.get('dest')
     backup = params.get('backup')
 
-    print(f"Module: copy, Src: {src}, Dest: {dest}, Backup: {backup}")
-
-    try:
-        sftp = client.open_sftp()
-
-        # Vérifier si la sauvegarde est activée
-        if backup:
-            # Chemin de sauvegarde (ajouter un timestamp au nom du fichier)
-            backup_dest = dest + '.bak'
-            sftp.rename(dest, backup_dest)
-
-        # Copier le fichier vers la destination distante
-        sftp.put(src, dest)
-        print("Fichier copié avec succès.")
-    except Exception as e:
-        print("Erreur lors de la copie du fichier:", str(e))
-    finally:
-        if sftp:
-            sftp.close()
+    # Vérifier si src est un fichier ou un dossier
+    if os.path.isfile(src):
+        # Copier un fichier
+        try:
+            scp = SCPClient(client.get_transport())
+            scp.put(src, dest)
+            scp.close()
+            print(f"File copied: {src} -> {dest}")
+        except Exception as e:
+            print(f"Error copying file: {e}")
+    elif os.path.isdir(src):
+        # Copier un dossier (récursivement)
+        try:
+            scp = SCPClient(client.get_transport())
+            scp.put(src, remote_path=dest, recursive=True)
+            scp.close()
+            print(f"Folder copied: {src} -> {dest}")
+        except Exception as e:
+            print(f"Error copying folder: {e}")
+    else:
+        print(f"Source path is invalid or does not exist: {src}")
