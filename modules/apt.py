dab98c80862ca106708e0b6336c8ecccc41bb79c
MAVINGAyannis <85069723+MAVINGAyannis@users.noreply.github.com>
2023-05-28 03:29:00 +0200

    clean and sort file

--- modules/apt.py
+++ modules/apt.py
@@ -1,24 +1,21 @@
 from modules.exec_command import execute_command
 
+def exec_apt(type, to_print, client, name):
+    # Faire quelque chose pour le module apt
+    command = "apt-get --yes " + type + " " + name
+    status = execute_command(client, command)
+    if status == 1:
+        print("APT " + to_print + " : COMMAND EXECUTED")
+
+    if status == 401:
+        print("APT " + to_print + " : WRONG COMMAND")
+
 def apt(client, params):
     name = params.get('name')
     state = params.get('state')
     print(f"Module: apt, Name: {name}, State: {state}")
 
     if state == 'present':
-        # Faire quelque chose pour le module apt
-        command = "apt-get --yes install " + name
-        status = execute_command(client, command)
-        if status == 1:
-            print("APT INSTALL : COMMAND EXECUTED")
-
-        if status == 401:
-            print("APT INSTALL : WRONG COMMAND")
+        exec_apt("install", "INSTALL", client, name)
     elif state == 'absent':
-        command = "apt-get --yes remove " + name
-        status = execute_command(client, command)
-        if status == 1:
-            print("APT REMOVE : COMMAND EXECUTED")
-
-        if status == 401:
-            print("APT REMOVE : WRONG COMMAND")
+        exec_apt("remove", "REMOVE", client, name)
