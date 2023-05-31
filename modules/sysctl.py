47d5345c7ea1e5c6da50db473d1c446a2d02f37f
MAVINGAyannis <85069723+MAVINGAyannis@users.noreply.github.com>
2023-05-31 06:05:17 +0200

    sysctl permanent modification saved and launched

--- modules/sysctl.py
+++ modules/sysctl.py
@@ -8,11 +8,12 @@ def sysctl(client, params):
     print(f"Module: sysctl, Attribute: {attribute}, Value: {value}, Permanent : {permanent}")
 
     if permanent:
-        command = f'sudo grep -q "{attribute}" /etc/sysctl.conf && sudo sed -i "s/{attribute}.*/{attribute}={value}/" /etc/sysctl.conf || echo "{attribute}={value}" | sudo tee -a /etc/sysctl.conf > /dev/null'
+        command = f'sudo grep -q "{attribute}" /etc/sysctl.conf && sudo sed -i "s/{attribute}.*/{attribute} = {value}/" /etc/sysctl.conf || echo "{attribute} = {value}" | sudo tee -a /etc/sysctl.conf > /dev/null'
     else:
         command = "sysctl " + attribute + "=" + str(value)
 
     if execute_command(client, command, "false", shell) == 1:
         print("SYCTL MODIFICATION : valid")
+        execute_command(client, "sysctl -p", "false", shell)
     else:
         print("SYSCTL MODIFICATION : error")
