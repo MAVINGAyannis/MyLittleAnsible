7fdeabb6ebbb01485c7fa5c38fcbf61e4fee4399
MAVINGAyannis <85069723+MAVINGAyannis@users.noreply.github.com>
2023-05-31 03:06:07 +0200

    delete useless comment

--- modules/service.py
+++ modules/service.py
@@ -5,23 +5,18 @@ def service(client, params):
     state = params.get('state')
 
     if state == 'started':
-        # Faire quelque chose pour le module service (state: started)
         print(f"Module: service, Name: {name}, State: {state}")
         command = "systemctl start " + name + ".service"
     elif state == 'restarted':
-        # Faire quelque chose pour le module service (state: enabled)
         print(f"Module: service, Name: {name}, State: {state}")
         command = "systemctl restart " + name + ".service"
     elif state == 'stopped':
-        # Faire quelque chose pour le module service (state: enabled)
         print(f"Module: service, Name: {name}, State: {state}")
         command = "systemctl stop " + name + ".service"
     elif state == 'enabled':
-        # Faire quelque chose pour le module service (state: enabled)
         print(f"Module: service, Name: {name}, State: {state}")
         command = "systemctl enable " + name + ".service"
     elif state == 'disabled':
-        # Faire quelque chose pour le module service (state: enabled)
         print(f"Module: service, Name: {name}, State: {state}")
         command = "systemctl disable " + name + ".service"
     else:
